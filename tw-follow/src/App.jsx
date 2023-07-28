e0417d0845b8179d6290afc139d7a656890f0d46
CamiloGiraldoM <camiloandresgiraldo000@gmail.com>
2023-07-28 12:37:08 -0500

    Consumo los datos a mostrar desde un array

--- tw-follow/src/App.jsx
+++ tw-follow/src/App.jsx
@@ -1,15 +1,43 @@
 import { TwitterFollowCard } from "./TwitterFollowCard.jsx";
 import "./App.css";
 
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Angel Duran',
+    isFollowing: true
+  },
+  {
+    userName: 'Xiaomi',
+    name: 'Xiaomi',
+    isFollowing: false
+  },
+  {
+    userName: 'reactjs',
+    name: 'React',
+    isFollowing: false
+  },
+  {
+    userName: 'platzi',
+    name: 'Platzi',
+    isFollowing: true
+  }
+]
+
 export function App() {
   return (
     <section className="App">
-        <TwitterFollowCard userName="midudev" >
-          Miguel Angel Duran
-        </TwitterFollowCard>
-        <TwitterFollowCard userName="pheralb">
-          Pablo Hernandez
-        </TwitterFollowCard>
+        {
+          users.map(({userName, name, isFollowing}) => (
+            <TwitterFollowCard 
+              userName={userName}
+              initialIsFollowing={isFollowing}
+            >
+              {name}
+            </TwitterFollowCard>
+            )
+          )
+        }
     </section>
   )
 }
